da9c3c6e8d2fb24a326d51bd7cafa7950e23c728
riwu <riwu0730@gmail.com>
2017-10-24 16:46:25 +0800

    Trim user input

--- client/src/actions/api.js
+++ client/src/actions/api.js
@@ -6,8 +6,8 @@ axios.defaults.baseURL = 'https://cs3219.ycholocron.com/';
 const get = path => axios.get(path).then(response => response.data);
 
 export default {
-  getTopStats: ({ count, type, venue }) => get(`venue/${encodeURIComponent(venue)}/${type.toLowerCase()}?top=${count}`),
-  getTrendStats: ({ type, venue }) => get(`venue/${encodeURIComponent(venue)}/${type.toLowerCase()}`),
-  getCitationWeb: ({ paper, depth }) => get(`paper/${encodeURIComponent(paper)}/web-citation?depth=${depth}`),
+  getTopStats: ({ count, type, venue }) => get(`venue/${encodeURIComponent(venue.trim())}/${type.toLowerCase()}?top=${count}`),
+  getTrendStats: ({ type, venue }) => get(`venue/${encodeURIComponent(venue.trim())}/${type.toLowerCase()}`),
+  getCitationWeb: ({ paper, depth }) => get(`paper/${encodeURIComponent(paper.trim())}/web-citation?depth=${depth}`),
   getTopVenues: ({ count, year }) => get(`year/${year}/avg-cite?top=${count}`),
 };
