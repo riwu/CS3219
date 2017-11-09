9e4c6ed4a29cc31e95b904d92963b41a1dd017cf
Shen Yichen <2007.yichen@gmail.com>
2017-11-09 14:23:09 +0800

    Refactor web-citation
    Fix bug in compare.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ server/db/web-citation.js
@@ -0,0 +1,54 @@
+const { connection, papersCollection } = require('./connection');
+
+async function queryRootPaper(title) {
+  const db = await connection;
+  const rootPapers = await db
+    .collection(papersCollection)
+    .aggregate([
+      { $match: { title } },
+      {
+        $project: {
+          id: 1, title: 1, inCitations: 1, authors: '$authors.name',
+        },
+      },
+      { $limit: 1 },
+    ]).toArray();
+
+  if (rootPapers.length === 0) {
+    return null;
+  }
+  return rootPapers[0];
+}
+
+async function queryGraph(rootPaperObjectId, depth) {
+  const db = await connection;
+  return db.collection(papersCollection)
+    .aggregate([
+      { $match: { _id: rootPaperObjectId } },
+      {
+        $graphLookup: {
+          from: 'a4papers',
+          startWith: '$inCitations',
+          connectFromField: 'inCitations',
+          connectToField: 'id',
+          maxDepth: depth - 1,
+          depthField: 'depth',
+          as: 'allCited',
+        },
+      },
+      { $project: { _id: 0, allCited: 1 } },
+      { $unwind: '$allCited' },
+      {
+        $project: {
+          id: '$allCited.id',
+          depth: '$allCited.depth',
+          title: '$allCited.title',
+          authors: '$allCited.authors.name',
+          inCitations: '$allCited.inCitations',
+        },
+      },
+    ])
+    .toArray();
+}
+
+module.exports = { queryRootPaper, queryGraph };
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const _ = require('lodash');
 const { connection, papersCollection } = require('../db/connection');
 const { queryCitationYearMap } = require('../db/compare');
+const { queryRootPaper, queryGraph } = require('../db/web-citation');
 
 const router = express.Router();
 
@@ -11,19 +12,42 @@ router.get('/compare', async (req, res) => {
   const conferences = _.castArray(req.query.conference);
   const years = _.castArray(req.query.year).map(yr => parseInt(yr, 10));
 
-  const conferenceYearObjs =
-    _(conferences)
-      .zip(years)
-      .map(pair => queryCitationYearMap(pair[0], pair[1]))
-      .value();
+  const conferenceYearObjs = _(conferences)
+    .zip(years)
+    .map(pair => ({ conference: pair[0], year: pair[1] }));
 
-  const formattedResults = _(await Promise.all(conferenceYearObjs))
-    .map(result => _.chain(result).keyBy('cite_year').mapValues('cite_count'))
-    .zip(conferenceYearObjs)
+  const results = conferenceYearObjs
+    .map(({ conference, year }) => queryCitationYearMap(conference, year))
+    .value();
+
+  const formattedResults = _(await Promise.all(results))
+    .map(result => _(result).keyBy('cite_year').mapValues('cite_count'))
+    .zip(conferenceYearObjs.value())
     .map(result => Object.assign({ citations: result[0] }, result[1]))
     .value();
 
   res.send(formattedResults);
+});
+
+// Citation Web
+router.get('/paper/:paper/web-citation', async (req, res) => {
+  const title = req.params.paper;
+  const { depth } = req.query;
+  const parsedDepth = depth === undefined ? 2 : parseInt(depth, 10);
+
+  const rootPaper = await queryRootPaper(title);
+
+  if (rootPaper === null) {
+    res.send(404);
+    return;
+  }
+
+  const webPapers = await queryGraph(rootPaper._id, parsedDepth);
+  const results = Object.assign(
+    { topId: rootPaper.id, [rootPaper.id]: rootPaper },
+    _(webPapers).keyBy('id').value(),
+  );
+  res.send(results);
 });
 
 // Q1
@@ -102,70 +126,6 @@ router.get('/venue/:venue/publications', async (req, res) => {
     .reduce((obj, entry) => Object.assign(obj, { [entry._id]: entry.publications }), {});
 
   res.send(response);
-});
-
-// Q4
-router.get('/paper/:paper/web-citation', async (req, res) => {
-  const title = req.params.paper;
-  const { depth } = req.query;
-  const parsedDepth = depth === undefined ? 2 : parseInt(depth, 10);
-
-  const db = await connection;
-
-  const rootPapers = await db
-    .collection(papersCollection)
-    .aggregate([
-      { $match: { title } },
-      {
-        $project: {
-          id: 1, title: 1, inCitations: 1, authors: '$authors.name',
-        },
-      },
-      { $limit: 1 },
-    ]).toArray();
-
-  if (rootPapers.length === 0) {
-    res.send(404);
-    return;
-  }
-
-  const rootPaper = rootPapers[0];
-
-  const webPapers = await db
-    .collection(papersCollection)
-    .aggregate([
-      { $match: { _id: rootPaper._id } },
-      {
-        $graphLookup: {
-          from: 'a4papers',
-          startWith: '$inCitations',
-          connectFromField: 'inCitations',
-          connectToField: 'id',
-          maxDepth: parsedDepth - 1,
-          depthField: 'depth',
-          as: 'allCited',
-        },
-      },
-      { $project: { _id: 0, allCited: 1 } },
-      { $unwind: '$allCited' },
-      {
-        $project: {
-          id: '$allCited.id',
-          depth: '$allCited.depth',
-          title: '$allCited.title',
-          authors: '$allCited.authors.name',
-          inCitations: '$allCited.inCitations',
-        },
-      },
-    ])
-    .toArray();
-
-  const results = webPapers
-    .reduce(
-      (obj, entry) => Object.assign(obj, { [entry.id]: entry }),
-      { topId: rootPaper.id, [rootPaper.id]: rootPaper },
-    );
-  res.send(results);
 });
 
 // Q5
