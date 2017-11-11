1ef82aad546aa3a22d916203bccec387d1f3d909
Shen Yichen <2007.yichen@gmail.com>
2017-11-12 00:05:56 +0800

    Drop old collection on import
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- server/deployment/db.sh
+++ server/deployment/db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mongoimport -d cs3219 -c a4papers < data.json
+mongoimport -d cs3219 -c a4papers --drop < data.json
 
 mongo cs3219 --eval "db.a4papers.createIndex({ id: 'hashed' })"
 mongo cs3219 --eval "db.a4papers.createIndex({ venue: 1, year: 1 })"
