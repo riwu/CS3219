2d3790eff45ec2bd452f9c303c203eb0e7752e6e
Shen Yichen <2007.yichen@gmail.com>
2017-11-13 10:30:12 +0800

    Add author.name index
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- server/deployment/db.sh
+++ server/deployment/db.sh
@@ -4,3 +4,4 @@ mongoimport -d cs3219 -c a5papers --drop < data.json
 
 mongo cs3219 --eval "db.a5papers.createIndex({ id: 'hashed' })"
 mongo cs3219 --eval "db.a5papers.createIndex({ venue: 1, year: 1 })"
+mongo cs3219 --eval "db.a5papers.createIndex({'authors.name': 1})"
