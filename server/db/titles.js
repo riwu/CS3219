b02f4ec32c64a008270a60c0848cd373811e1916
Shen Yichen <2007.yichen@gmail.com>
2017-11-13 15:16:01 +0800

    Add sort
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- server/db/titles.js
+++ server/db/titles.js
@@ -4,7 +4,11 @@ async function queryTitles() {
   const db = await connection;
 
   return db.collection(papersCollection)
-    .aggregate([{ $group: { _id: '$title' } }])
+    .aggregate([
+      { $group: { _id: '$title' } },
+      { $match: { _id: { $nin: [null, ''] } } },
+      { $sort: { _id: 1 } },
+    ])
     .toArray();
 }
 
--- server/db/venues.js
+++ server/db/venues.js
@@ -4,7 +4,11 @@ async function queryVenues() {
   const db = await connection;
 
   return db.collection(papersCollection)
-    .aggregate([{ $group: { _id: '$venue' } }])
+    .aggregate([
+      { $group: { _id: '$venue' } },
+      { $match: { _id: { $nin: [null, ''] } } },
+      { $sort: { _id: 1 } },
+    ])
     .toArray();
 }
 
